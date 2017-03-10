e2cbb37b0779c0a5f87ca70ff544a12d32e669c4
AminaKhalid <aminakhalid4@hotmail.com>
2017-03-10 21:10:50 +0000

    complete

--- comp10050/game.c
+++ comp10050/game.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-
 struct data
 	{
 		int points;
@@ -13,12 +12,20 @@ struct data
 		int magicSkills;
 		int luck;
 	};
-struct user
+
+struct slots//struct of slots
+{
+	char type[50];//stores type of slot
+	int slotNum;//the position of the slot ie 0 = the first position in the slot
+	int typeNum;//stores the slot type as a number between 1-3
+
+};
+/*struct user
 {
 	char name[20];
 	int type;
 	
-};
+};*/
 //function declaration
 void ogre(struct data);
 void elf(struct data);
@@ -40,9 +47,9 @@ int main()
 	int slots;
 	char name [50];
 	
-	struct user info[40];
+	//struct user info[40];
 	struct data player;
-
+	struct slots slot[50];
 	
 	printf("How many players would you like to select (max 6)? ");
 	scanf("%d", &p);
@@ -59,7 +66,6 @@ int main()
 		{
 		case 1:
 		human(player);
-		info[i].type = ;
 		break;
 		case 2:
 		ogre(player);
@@ -77,25 +83,25 @@ int main()
 	scanf("%d", &s);
 	for(i=0;i<s;i++)
 	{
-		random = 0 + rand() % 3;
+		random = 0 + rand() % 3;//randomly choosing a number between 1 and 3
 		
 		if (random == 0)
 		{
-			printf(" level ground");
+			strcpy(slot[i].type, "level ground"); //if random the random number equals 0, then copy level ground into slot type
 		}
 		if (random == 1)
 		{
-			printf(" hill");
+			strcpy(slot[i].type, "hill"); //if random the random number equals 1, then copy hill into slot type
 		}
 		if (random == 2)
 		{
-			printf(" city");
+			strcpy(slot[i].type, "city"); //if random the random number equals 2, then copy city into slot type
 		}
 		
-		printf("\n%d", i);
+		printf("\n%d %s", i+1, slot[i].type); //print the slot number and the randomly assigned slot type
 	}
 
-	
+	//option to move or attack
 	for(i=0; i<p; i++)
         {
             printf("Player %d Press 1 to move or 2 to attack", i+1);
@@ -128,7 +134,7 @@ void ogre(struct data o)
 
 	srand (time(NULL));
 
-	opoints = 100;
+	o.points = 100;
 	o.smartness = 1 + rand() % 20;
 	o.strength = 80 + rand() % 20;
 	o.dexterity = 80 + rand() %20;
@@ -163,14 +169,12 @@ void wizard(struct data w)
 	printf("\nWIZARD: lifepoints: %d\tsmartness: %d\tstrength: %d\tdexterity: %d\tmagicSkills: %d\tluck: %d\n", w.points, w.smartness, w.strength, w.dexterity, w.magicSkills, w.luck);
 }
 
-/*void movePlayers()
+void movePlayers()
 {
 	int victim;
-	/*If a player (Attacker) decides to attack the closest player (Attacked)
-If the Strength points of the attacked player are <=70, then attacked player life points = life points - 0.5 * his/her Strength points. 
-If the strength points of the attacked player are > 70, the attacker life points = life points - 0.3 * (attacked player’s strength points).
-
-printf("press 1 to attack the closest player: ");
+	/*If the Strength points of the victim are <=70, then attacked player life points = life points - 0.5 * his/her Strength points. 
+	If the strength points of the victim are > 70, the attacker life points = life points - 0.3.*/
+	printf("press 1 to attack the closest player: ");
 	if (victim.strength <=70)
 	{
 		victim.points = victim.points - (0.5 * (victim.strength));
@@ -180,4 +184,3 @@ printf("press 1 to attack the closest player: ");
 		victim.points = victim.points - (0.3 * (victim.strength));
 	}
 }
-*/
